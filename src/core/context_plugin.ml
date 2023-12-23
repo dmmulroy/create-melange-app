539e26bd80a48bac767f87a626959b049c2b9249
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-12-23 12:44:22 -0500

    work on Context V2

--- src/core/context_plugin.ml
+++ src/core/context_plugin.ml
@@ -167,9 +167,43 @@ module V2 = struct
     template_values : Hmap.t;
   }
 
+  type template_instance =
+    | TemplateInstance :
+        (module Template.S with type t = 'a) * 'a
+        -> template_instance
+
+  let base_templates : template_instance list =
+    [
+      TemplateInstance
+        ( (module Package_json.Template : Template.S with type t = Package_json.t),
+          Package_json.empty );
+      TemplateInstance
+        ( (module Dune_project.Template : Template.S with type t = Dune_project.t),
+          Dune_project.empty );
+    ]
+  ;;
+
   let make ?(templates = String_map.empty) ?(template_values = Hmap.empty)
       ~configuration () =
     { configuration; templates; template_values }
+  ;;
+
+  let of_configuration (configuration : Configuration.t) =
+    List.fold_left
+      (fun ctx (TemplateInstance (template, empty_value)) ->
+        let module TemplateInstance =
+          (val template : Template.S with type t = 'a)
+        in
+        {
+          ctx with
+          templates =
+            String_map.add TemplateInstance.name
+              (module TemplateInstance : Template.S)
+              ctx.templates;
+          template_values =
+            Hmap.add TemplateInstance.key empty_value ctx.template_values;
+        })
+      (make ~configuration ()) base_templates
   ;;
 
   let get_template_by_name ~name ctx = String_map.find_opt name ctx.templates
