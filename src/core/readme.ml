6039b225c33821985b6f181f92320f4b9b51dc0b
Dillon Mulroy <dillon.mulroy@gmail.com>
2024-01-04 21:40:53 -0500

    Update readme template

--- src/core/readme.ml
+++ src/core/readme.ml
@@ -2,6 +2,6 @@ type t = Configuration.t
 
 let template (configuration : Configuration.t) : t Template.t =
   let template_directory = Node.Path.join [| configuration.directory; "./" |] in
-  Template.make ~name:"README.tmpl" ~value:configuration ~dir:template_directory
-    ~to_json:Configuration.to_json
+  Template.make ~name:"README.md.tmpl" ~value:configuration
+    ~dir:template_directory ~to_json:Configuration.to_json
 ;;
