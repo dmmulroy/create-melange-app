a9d5a79a7ebabaffa7c2365f4740d87a19f9c2b9
JoaoAlexNunes <57943137+JoaoAlexNunes@users.noreply.github.com>
2024-01-10 14:10:13 +0100

    I tried to get the correct package manager (#65)
    * fixed readme template for showing the correct node package manager
    
    * I think the correct is the node_package_manager_str version
    
    * Correct way of displaying the package manager
    
    * Added react-book-.melange.re to README template resources
    
    * update readme
    
    * remove prtest
    
    ---------
    
    Co-authored-by: Dillon Mulroy <dillon.mulroy@gmail.com>

--- src/core/readme.ml
+++ src/core/readme.ml
@@ -1,7 +1,28 @@
+open Bindings
+
 type t = Configuration.t
+
+let to_json (readme : Configuration.t) =
+  let dict = Js.Dict.empty () in
+  Js.Dict.set dict "name" (Js.Json.string readme.name);
+  Js.Dict.set dict "directory" (Js.Json.string readme.directory);
+  Js.Dict.set dict "node_package_manager"
+    (Js.Json.string
+       (readme.node_package_manager |> Nodejs.Process.npm_user_agent_to_string));
+  Js.Dict.set dict "syntax_preference"
+    (Js.Json.string
+       (Configuration.syntax_preference_to_string readme.syntax_preference));
+  Js.Dict.set dict "bundler"
+    (Js.Json.string
+       (Bundler.to_string readme.bundler |> String.capitalize_ascii));
+  (* We add a derived field to feed into our template for convenience *)
+  Js.Dict.set dict "is_webpack_app"
+    (Js.Json.boolean (readme.bundler == Webpack));
+  Js.Json.object_ dict
+;;
 
 let template (configuration : Configuration.t) : t Template.t =
   let template_directory = Node.Path.join [| configuration.directory; "./" |] in
   Template.make ~name:"README.md.tmpl" ~value:configuration
-    ~dir:template_directory ~to_json:Configuration.to_json
+    ~dir:template_directory ~to_json
 ;;
